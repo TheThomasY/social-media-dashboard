a8a27b678ca2997eb30830a01b1e0da7da5a7401
TheThomasY <twy1g16@soton.ac.uk>
2022-01-12 16:44:57 +0000

    feat: added dark mode toggle to header, ready for new components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
+import React, { useState } from 'react';
 import Header from './components/Header';
-
 import './App.css';
 
 function App() {
+  const [darkMode, setDarkMode] = useState(false);
+
+  const toggleDarkMode = () => {
+    setDarkMode(!darkMode);
+  };
+
   return (
-    <div className='font-Inter'>
-      <Header />
+    <div className={'font-Inter' + (darkMode ? ' dark' : '')}>
+      <Header onToggleDarkMode={toggleDarkMode} colourMode={darkMode} />
     </div>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,9 +7,16 @@ module.exports = {
   theme: {
     extend: {
       colors: {
+        // Light scheme
         'grey-text': 'hsl(228, 12%, 44%)',
         'dark-text': 'hsl(230, 17%, 14%)',
         'grey-line': '#898c9b',
+
+        // Dark scheme
+        'dk-bg': 'hsl(230, 17%, 14%)',
+        'dk-pattern-bg': 'hsl(232, 19%, 15%)',
+        'dk-card-bg': 'hsl(228, 28%, 20%)',
+        'dk-text': 'hsl(228, 34%, 66%)',
       },
       fontFamily: {
         'Inter': ['Inter', 'sans-serif']
