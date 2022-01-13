002775948a572db1a594baf46758cd7687256993
TheThomasY <twy1g16@soton.ac.uk>
2022-01-13 17:31:26 +0000

    feat: data change arrow now updates colour, red - down, green -up

--- src/components/BigCard.js
+++ src/components/BigCard.js
@@ -1,6 +1,14 @@
 import React from 'react';
 
+import upArrow from '../images/icon-up.svg';
+import downArrow from '../images/icon-down.svg';
+
 export default function BigCard(props) {
+  let arrowColour =
+    props.socialData.followerChange[0] === '+'
+      ? 'hsl(163, 72%, 41%)'
+      : 'hsl(356, 69%, 56%)';
+
   return (
     <div
       className='w-full mt-7 lg:mt-12 py-7 text-center border-t-4 rounded-md bg-card-bg dark:bg-dk-card-bg'
@@ -26,8 +34,15 @@ export default function BigCard(props) {
       <div className='mt-3 text-grey-text dark:text-dk-text tracking-widest'>
         {props.socialData.followerWord}
       </div>
-      <div className='mt-7 text-sm font-bold text-up-green'>
-        {props.socialData.followerChange} Today
+      <div className='mt-7 flex justify-center items-center gap-x-1'>
+        {props.socialData.followerChange[0] === '+' ? (
+          <img src={upArrow} alt='' />
+        ) : (
+          <img src={downArrow} alt='' />
+        )}
+        <div style={{ color: arrowColour }} className='text-sm font-bold'>
+          {props.socialData.followerChange.slice(1)} Today
+        </div>
       </div>
     </div>
   );
--- src/components/SmallCard.js
+++ src/components/SmallCard.js
@@ -1,6 +1,14 @@
 import React from 'react';
 
+import upArrow from '../images/icon-up.svg';
+import downArrow from '../images/icon-down.svg';
+
 export default function SmallCard(props) {
+  let arrowColour =
+    props.data.dataChange[0] === '+'
+      ? 'hsl(163, 72%, 41%)'
+      : 'hsl(356, 69%, 56%)';
+
   return (
     <div className='w-full mt-4 p-6 rounded-md font-bold bg-card-bg dark:bg-dk-card-bg'>
       <div className='mt-1 flex justify-between items-center'>
@@ -11,7 +19,16 @@ export default function SmallCard(props) {
       </div>
       <div className='mt-7 flex justify-between items-end'>
         <div className='text-3xl dark:text-white'>{props.data.data}</div>
-        <div className='text-up-green'>{props.data.dataChange}</div>
+        <div className='flex justify-center items-center gap-x-1'>
+          {props.data.dataChange[0] === '+' ? (
+            <img src={upArrow} alt='' />
+          ) : (
+            <img src={downArrow} alt='' />
+          )}
+          <div style={{ color: arrowColour }}>
+            {props.data.dataChange.slice(1)}
+          </div>
+        </div>
       </div>
     </div>
   );
